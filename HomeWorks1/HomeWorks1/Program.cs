8fe7c54eae1ce28ee2b3e3976a69e5f6c871e682
MAXGODDYK <125493210+MAXGODDYK@users.noreply.github.com>
2025-06-13 01:04:48 +0300

    ad nev sr

--- HomeWorks1/HomeWorks1/Program.cs
+++ HomeWorks1/HomeWorks1/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 Console.WriteLine("$$$_____$$$$$$$_$$$$$$$_$$$_______$$$_$$$$$$$$$$");
 Console.WriteLine("$$$____$$$____$$$____$$$_$$$_____$$$__$$$_______");
 Console.WriteLine("$$$____$$$_____$_____$$$_$$$_____$$$__$$$_______");
---
+++ HomrWork2/HomrWork2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int var = 52;
+Console.WriteLine((float)var / 10);
+Console.WriteLine(var - (var / 10) * 10);
