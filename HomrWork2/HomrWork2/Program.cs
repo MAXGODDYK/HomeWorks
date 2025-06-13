ca8d922d01d346cbd113084dd227f94658ef3374
MAXGODDYK <125493210+MAXGODDYK@users.noreply.github.com>
2025-06-13 22:28:35 +0300

    DzNom2

--- HomrWork2/HomrWork2/Program.cs
+++ HomrWork2/HomrWork2/Program.cs
@@ -1,4 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int var = 52;
-Console.WriteLine((float)var / 10);
-Console.WriteLine(var - (var / 10) * 10);
+int var = 12345;
+
+int v1 = var / 10000; 
+int v2 = (var / 1000) % 10; 
+int v3 = (var / 100) % 10; 
+int v4 = (var / 10) % 10; 
+int v5 = var % 10; 
+
+Console.WriteLine(var); 
+
+Console.WriteLine(v1);
+Console.WriteLine(v2);
+Console.WriteLine(v3);
+Console.WriteLine(v4);
+Console.WriteLine(v5);
+
+
+Console.WriteLine("Ведите массив: ");
+int[] faifthMas = new int[5];
+for (int i = 0; i < 5; i++) 
+{
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    faifthMas[i] = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine("Вывод массива: ");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(faifthMas[i]);
+}
+
+int numbersMin = 0;
+int numbersMax = 0;
+for (int i = 1; i < 5; i++)
+{
+    numbersMin = faifthMas[0];
+    if (numbersMin > faifthMas[i])
+    {
+        numbersMin = faifthMas[i];
+    }
+}
+Console.WriteLine("Min: " + numbersMin);
+for (int i = 1; i < 5; i++)
+{
+    numbersMax = faifthMas[0];
+    if (numbersMax < faifthMas[i])
+    {
+        numbersMax = faifthMas[i];
+    }
+}
+Console.WriteLine("Max: " + numbersMax);
+
