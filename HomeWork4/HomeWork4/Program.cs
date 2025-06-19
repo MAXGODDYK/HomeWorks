78516a12a4b39b67a2f1075ac7ffb29c6818ae7e
MAXGODDYK <125493210+MAXGODDYK@users.noreply.github.com>
2025-06-19 06:37:13 +0300

    Homework5

---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] mynum = new int[10];
+Console.WriteLine("Enter 10 numbers:");
+
+
+for (int i = 0; i < mynum.Length; i++)
+{
+    Console.Write($"Number {i + 1}: ");
+    mynum[i] = int.Parse(Console.ReadLine());
+}
+
+
+Console.WriteLine("Enter the number we are looking for:");
+int yuernum = int.Parse(Console.ReadLine());
+
+int count = 0;
+for (int i = 0; i < mynum.Length; i++)
+{
+    if (mynum[i] == yuernum)
+    {
+        Console.WriteLine($"Number in place {i+=1}");
+        count++;
+    }
+}
+if (count == 0)
+{
+    Console.WriteLine("Number not found");
+}
+else
+{
+    Console.WriteLine($"Number found {count} times");
+}
+for (int i = 0; i < mynum.Length; i++)
+{
+    Console.WriteLine(mynum[mynum.Length - 1 - i]);
+}
+
+
+
